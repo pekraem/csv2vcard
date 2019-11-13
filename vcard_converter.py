f8afba22aff102837b0637b15193ab1518b8fa9f
Peter Krämer <peter.kramer@cern.ch>
2019-11-13 23:28:35 +0100

    conversion works

--- vcard_converter.py
+++ vcard_converter.py
@@ -1,5 +1,78 @@
+import pandas as pd
+
+df = pd.read_csv("~/Downloads/adressliste.csv",";",header=1)
+df=df.replace("E-mail", "email")
 
 
+#print df.Adresse.to_string().split()
+#print df["E-Mail"]
+#print df
+
+cards = []
+geb_d = []
+geb_m = []
+geb_y = []
+
+
+for i in range(len(df)):
+    #print df.Adresse[i].replace(","," ").split()
+    if len(df.Adresse[i])<4:
+        df.Adresse[i]="Str Nr Stadt plz"
+    if df.Geburtstdatum[i]:
+        geb_d.append((df.Geburtstdatum[i].split(".")[0]))
+        geb_m.append((df.Geburtstdatum[i].split(".")[1]))
+        geb_y.append((df.Geburtstdatum[i].split(".")[2]))
+    vcard = '''
+BEGIN:VCARD
+VERSION:2.1
+N:{Nachname};{vname};
+FN:{vname} {Nachname}
+TEL;TYPE=CELL,VOICE:{handy}
+ADR;HOME:;;{street} {nr};{town};;{plz};{country}
+EMAIL:{email}
+BDAY:{geb}
+END:VCARD
+'''.format(Nachname=df.Nachname[i],
+                vname=df.Vorname[i],
+                handy=df.Handynummer[i],
+                #street="s",
+                #nr="1",
+                #town="e",
+                #plz="2",
+                #email="JB",
+                street=df.Adresse[i].replace(","," ").split()[0],
+                nr=df.Adresse[i].replace(","," ").split()[1],
+                town=df.Adresse[i].replace(","," ").split()[2],
+                plz=df.Adresse[i].replace(","," ").split()[3],
+                country="Deutschland",
+                email=df["E-Mail"][i],
+                geb=df.Geburtstdatum[i])
+    cards.append(vcard)
+
+df["vcard"] = cards
+df["geb_d"] = geb_d
+df["geb_m"] = geb_m
+df["geb_y"] = geb_y
+
+try:
+    df["day"] = df["geb_d"].astype(int)
+    df["year"] = df["geb_y"].astype(int)
+except:
+    df["day"] = df["geb_d"].astype(int)
+df["month"] = df["geb_m"].astype(int)
+
+#print df[["geb_d","geb_m","geb_y"]]
+print df[df.month>=11].Vorname
+
+string = ""
+string = string.join(df["vcard"])
+string.replace("    ","")
+
+df.to_hdf('KJG_Adressen.h5', key='df', mode='w')
+
+print string
+with open("KJG_vcard.vcf","a") as vc:
+    vc.write(string)
 
 class contact:
     """A class to store a contact in a dictionary and provide several functions to add, remove, change or export contacts"""
